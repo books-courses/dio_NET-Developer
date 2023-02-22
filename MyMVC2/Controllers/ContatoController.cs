f4d10298d501fbfea1a41f27c124586e92512398
Michel Metran <michelmetran@gmail.com>
2023-02-22 20:48:53 -0300

    Ajustes

--- MyMVC2/Controllers/ContatoController.cs
+++ MyMVC2/Controllers/ContatoController.cs
@@ -1,6 +1,87 @@
+using Microsoft.AspNetCore.Mvc;
+using MyMVC2.Context;
+using MyMVC2.Models;
+
+
 namespace MyMVC2.Controllers;
 
-public class ContatoController
+public class ContatoController : Controller
 {
-    
+    private readonly AgendaContext _context;
+
+    public ContatoController(AgendaContext context)
+    {
+        _context = context;
+    }
+
+    public IActionResult Index()
+    {
+        var contatos = _context.Contatos.ToList();
+        return View(contatos);
+    }
+
+    public IActionResult Criar()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Criar(Contato contato)
+    {
+        if (ModelState.IsValid)
+        {
+            _context.Contatos.Add(contato);
+            _context.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+
+        return View(contato);
+    }
+
+    [HttpGet]
+    public IActionResult Editar(int id)
+    {
+        var contato = _context.Contatos.Find(id);
+        if (contato == null)
+            return NotFound();
+        return View(contato);
+    }
+
+    [HttpPost]
+    public IActionResult Editar(Contato contato)
+    {
+        var contatoBanco = _context.Contatos.Find(contato.Id);
+        contatoBanco.Nome = contato.Nome;
+        contatoBanco.Telefone = contato.Telefone;
+        contatoBanco.Ativo = contato.Ativo;
+
+        _context.Contatos.Update(contatoBanco);
+        _context.SaveChanges();
+        return RedirectToAction(nameof(Index));
+    }
+
+    public IActionResult Detalhes(int Id)
+    {
+        var contato = _context.Contatos.Find(Id);
+        if (contato == null)
+            return RedirectToAction(nameof(Index));
+        return View(contato);
+    }
+
+    public IActionResult Deletar(int Id)
+    {
+        var contato = _context.Contatos.Find(Id);
+        if (contato == null)
+            return RedirectToAction(nameof(Index));
+        return View(contato);
+    }
+
+    [HttpPost]
+    public IActionResult Deletar(Contato contato)
+    {
+        var contatoBanco = _context.Contatos.Find(contato.Id);
+        _context.Contatos.Remove(contatoBanco);
+        _context.SaveChanges();
+        return RedirectToAction(nameof(Index));
+    }
 }
